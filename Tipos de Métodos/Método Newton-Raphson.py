718a168987d867a81dc97dff1279e30c73faa054
Ale-maroto <alejandromarotomay@gmail.com>
2026-03-30 15:14:12 -0600

    Update Método Newton-Raphson.py

--- Tipos de Métodos/Método Newton-Raphson.py
+++ Tipos de Métodos/Método Newton-Raphson.py
@@ -5,7 +5,13 @@ Created on Mon Mar 23 16:11:25 2026
 @author: Alejandro
 """
 
+#Versión 2.0.0
+# Agrege el codigo para la grafica y tambien agrege lo de las funciones matematicas
+
 import sympy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+import math
 
 def newton_raphson():
 
@@ -20,10 +26,17 @@ def newton_raphson():
 
     # Convertir función
     f_expr = sp.sympify(funcion)
-    f = sp.lambdify(x, f_expr)
 
-    # Derivar automáticamente
+    # Función con soporte matemático
+    def f(val):
+        return eval(funcion, {"x": val, "math": math,
+                             "sin": math.sin, "cos": math.cos, "tan": math.tan,
+                             "exp": math.exp, "log": math.log, "sqrt": math.sqrt})
+
+    # Derivada simbólica
     df_expr = sp.diff(f_expr, x)
+
+    # Convertir derivada a función evaluable
     df = sp.lambdify(x, df_expr)
 
     print("\nDerivada calculada:")
@@ -34,6 +47,10 @@ def newton_raphson():
 
     print("\nIter |    x_n     |   f(x_n)   |   f'(x_n)  |    Ea %")
     print("---------------------------------------------------------")
+
+    # Guardar iteraciones
+    x_vals_iter = []
+    fx_vals_iter = []
 
     while ea > error_max:
 
@@ -51,11 +68,35 @@ def newton_raphson():
 
         print(f"{iteracion:4} | {x0:10.6f} | {f(x0):10.6f} | {df(x0):10.6f} | {ea:10.6f}")
 
+        # Guardar puntos
+        x_vals_iter.append(x0)
+        fx_vals_iter.append(f(x0))
+
         x0 = x1
         iteracion += 1
 
     print("\nRaiz aproximada:", x1)
     print("Error aproximado:", ea, "%")
+
+    # -------- GRAFICA --------
+    x_vals = np.linspace(x0-5, x0+5, 200)
+    y_vals = [f(val) for val in x_vals]
+
+    plt.plot(x_vals, y_vals, label="f(x)")
+    plt.axhline(0)
+
+    # Puntos de iteraciones
+    plt.scatter(x_vals_iter, fx_vals_iter, label="Iteraciones")
+
+    plt.title("Método de Newton-Raphson")
+    plt.xlabel("x")
+    plt.ylabel("f(x)")
+    plt.legend()
+    plt.grid()
+
+    plt.show()
+
+newton_raphson()
 
 
 # Ejecutar
